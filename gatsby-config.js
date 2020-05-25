7e20a07b407dcd83c9c4f10aa61ef081ac794eba
eeduplay <gherrerarausaure@gmail.com>
2020-05-24 22:14:10 -0400

    configure typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Interstellar Flight`,
+    subtitle: `Experimental Research Group`,
+    description: ``,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -25,6 +26,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography"
+
+const typography = new Typography({
+    headerFontFamily: ['Exo', 'Helvetica Neue', 'sans-serif'],
+    bodyFontFamily: ['PT Sans', 'sans-serif'],
+})
+
+export default typography
