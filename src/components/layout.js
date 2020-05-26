0795c3d79c4e851ba186ccb7d6e5b3964bae6f04
eeduplay <gherrerarausaure@gmail.com>
2020-05-25 21:02:44 -0400

    finish header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import logo from "../images/styling/logo.png"
+import mcgill from "../images/styling/mcgillLogo.svg"
+
+import headerStyle from "./styles/header.module.css"
+import layoutStyle from "./styles/layout.module.css"
+
+const Header = props => (
+    <header>
+        <img src={logo} alt="Logo" height="128" className={headerStyle.logo}/>
+        <div className={headerStyle.titleblock}>
+            <a href="https://mcgill.ca" className={headerStyle.mcgill} target="_blank">
+                <img src={mcgill} alt="McGill Logo" height="20" />
+            </a>
+            <Link to="/"><h1>{props.title}</h1></Link>
+            <h3>{props.subtitle}</h3>
+        </div>
+    </header>
+)
 
 export default function Layout({ children }){
+    const data = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                        subtitle
+                    }
+                }
+            }
+        `
+    )
     return (
-        <div style={{ width: `1280px`, margin: `0 auto`, backgroundColor: `#191D2B` }}>
+        <div className={layoutStyle.container}>
+            <Header title={data.site.siteMetadata.title} subtitle={data.site.siteMetadata.subtitle} />
             {children}
         </div>
     )
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <h2>Header 2</h2>
       <h3>Header 3</h3>
       <h4>Monday May 5, 2020</h4>
-      <p>Lorem ipsum dolor sit amet,</p>
+      <p>Lorem ipsum dolor sit amet, <a href="https://google.com">this</a> is a link.</p>
     </Layout>
   )
 }
