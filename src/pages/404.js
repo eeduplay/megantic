d502e0491bf5f2e1ef2527c997c8c40a9e1b72ca
eeduplay <gherrerarausaure@gmail.com>
2020-05-26 18:24:11 -0400

    sexier 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 
+import spaceSign from "../images/styling/404.svg"
+
 export default function NotFound() {
   return (
     <Layout>
-      <h2>Woops!</h2>
+      <img src={spaceSign} alt="" style={{ width: `70%`, height: `auto`, display: `block`, margin: `auto` }}/>
+      <h2>Whoops!</h2>
       <h4>404: Page not found</h4>
       <p>Looks like you got lost in the interstellar medium.</p>
       <p><Link to="/">Back to the launchpad</Link></p>
