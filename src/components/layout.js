862422a5431f6e9275919cb5c165e27033256dfb
eeduplay <gherrerarausaure@gmail.com>
2020-05-26 12:40:48 -0400

    add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,19 +4,27 @@ import logo from "../images/styling/logo.png"
 import mcgill from "../images/styling/mcgillLogo.svg"
 
 import headerStyle from "./styles/header.module.css"
+import footerStyle from "./styles/footer.module.css"
 import layoutStyle from "./styles/layout.module.css"
 
 const Header = props => (
     <header>
         <img src={logo} alt="Logo" height="128" className={headerStyle.logo}/>
         <div className={headerStyle.titleblock}>
-            <a href="https://mcgill.ca" className={headerStyle.mcgill} target="_blank">
+            <a href="https://mcgill.ca" className={headerStyle.mcgill} target="_blank" rel="noreferrer">
                 <img src={mcgill} alt="McGill Logo" height="20" />
             </a>
             <Link to="/"><h1>{props.title}</h1></Link>
             <h3>{props.subtitle}</h3>
         </div>
     </header>
+)
+
+const Footer = props => (
+    <footer className={footerStyle.footer}>
+        <p>Unless otherwise stated, all media presented is licensed under <a href="https://creativecommons.org/licenses/by/4.0" target="_blank" rel="noreferrer">CC BY 4.0</a></p>
+        <p>Latest build on {props.buildDate}</p>
+    </footer>
 )
 
 export default function Layout({ children }){
@@ -29,13 +37,18 @@ export default function Layout({ children }){
                         subtitle
                     }
                 }
+                siteBuildMetadata {
+                    buildTime(formatString: "MMMM DD, YYYY")
+                }
             }
         `
     )
+    console.log(data.siteBuildMetadata.buildTime)
     return (
         <div className={layoutStyle.container}>
             <Header title={data.site.siteMetadata.title} subtitle={data.site.siteMetadata.subtitle} />
             {children}
+            <Footer buildDate={data.siteBuildMetadata.buildTime} />
         </div>
     )
 }
