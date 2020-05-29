8f4e4103664a4035af61305a6d4d25af7b6d5ff2
eeduplay <gherrerarausaure@gmail.com>
2020-05-29 10:12:37 -0400

    applied layout changes to all pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,15 @@ import Layout from "../components/layout"
 export default function Home(props) {
   return (
     <Layout current={props.location.pathname}>
-      <h2>Per Aspera Ad Astra</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mi augue, euismod porta dapibus ut, egestas sodales eros.</p>
-      <p>Vivamus ullamcorper mi at leo tincidunt, vel convallis dui rutrum. Nam nec posuere nulla. Ut vel molestie leo. Suspendisse vel urna quis tellus ultricies tristique. Phasellus eget orci tristique nibh finibus bibendum. Praesent sit amet elementum libero. </p>
-      <h2>Latest Posts</h2>
-      <h3>First Post!</h3>
-      <h4>Monday May 5, 2020</h4>
-      <p>Lorem ipsum dolor sit amet, <a href="https://google.com">this</a> is a link.</p>
+      <main>
+        <h2>Per Aspera Ad Astra</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mi augue, euismod porta dapibus ut, egestas sodales eros.</p>
+        <p>Vivamus ullamcorper mi at leo tincidunt, vel convallis dui rutrum. Nam nec posuere nulla. Ut vel molestie leo. Suspendisse vel urna quis tellus ultricies tristique. Phasellus eget orci tristique nibh finibus bibendum. Praesent sit amet elementum libero. </p>
+        <h2>Latest Posts</h2>
+        <h3>First Post!</h3>
+        <h4>Monday May 5, 2020</h4>
+        <p>Lorem ipsum dolor sit amet, <a href="https://google.com">this</a> is a link.</p>
+      </main>
     </Layout>
   )
 }
