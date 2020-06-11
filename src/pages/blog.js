ce8a9fae4ddd537693512a60ed5cecd50fe9b26e
Zhuo Fan Bao <zhuofanb@gmail.com>
2020-06-11 11:16:40 -0400

    initialized automatic page creation for blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import { node } from "prop-types"
 
 export default function Blog(props) {
   return (
     <Layout current={props.location.pathname}>
       <main>
-        <h2>Blog</h2>
+        <h1>Blog</h1>
+        
+        {props.data.allMarkdownRemark.nodes.map(nodes => (
+          <div key={nodes.id}>
+            <h2>
+              <Link
+                to={nodes.fields.slug}
+              >
+                {nodes.frontmatter.title}
+              </Link>
+            </h2>
+            <h4>{nodes.frontmatter.author} - {nodes.frontmatter.date}</h4>
+            <p>{nodes.excerpt}</p>
+          </div>
+        ))}
       </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query{
+    allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/blog/"}}, sort: {order: DESC, fields: frontmatter___date}) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "dddd, MMMM DD, YYYY")
+          author
+        }
+        fields {
+          slug
+        }
+        excerpt
+        id
+      }
+    }
+  }
+`
