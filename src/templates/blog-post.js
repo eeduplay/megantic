600ded5e130ad01b76429faa0080e4c9489e7608
Zhuo Fan Bao <zhuofanb@gmail.com>
2020-06-11 15:40:03 -0400

    wrap blog posts in main

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,11 +6,13 @@ export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
+      <main>
       <div>
         <h1>{post.frontmatter.title}</h1>
         <h4>By {post.frontmatter.author} - {post.frontmatter.date}</h4>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
+      </main>
     </Layout>
   )
 }
