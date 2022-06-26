5925771196b23f541dd23f8f475b38392e007203
Emmanuel Duplay <emmanuel.duplay@gmail.com>
2022-06-26 15:16:30 -0400

    cleanup and style changes

--- src/components/social.js
+++ src/components/social.js
@@ -11,11 +11,11 @@ export default function SocialLinks ( props ) {
     const isGit = Boolean(props.git);
     return (
         <div className={socialStyles.container}>
-            {isEmail && <a target="_blank" rel="noreferrer" href={"mailto:" + props.email} className={[socialStyles.email, socialStyles.button].join(" ")}></a>}
-            {isWebsite && <a target="_blank" rel="noreferrer" href={props.website} className={[socialStyles.website, socialStyles.button].join(" ")}></a>}
-            {isLinkedin && <a target="_blank" rel="noreferrer" href={props.linkedin} className={[socialStyles.linkedin, socialStyles.button].join(" ")}></a>}
-            {isGit && <a target="_blank" rel="noreferrer" href={props.git} className={[socialStyles.git, socialStyles.button].join(" ")}></a>}
-            {isTwitter && <a target="_blank" rel="noreferrer" href={props.twitter} className={[socialStyles.twitter, socialStyles.button].join(" ")}></a>}
+            {isEmail && <a target="_blank" rel="noreferrer" href={"mailto:" + props.email} className={[socialStyles.email, socialStyles.button].join(" ")}>Email</a>}
+            {isWebsite && <a target="_blank" rel="noreferrer" href={props.website} className={[socialStyles.website, socialStyles.button].join(" ")}>Website</a>}
+            {isLinkedin && <a target="_blank" rel="noreferrer" href={props.linkedin} className={[socialStyles.linkedin, socialStyles.button].join(" ")}>LinkedIn</a>}
+            {isGit && <a target="_blank" rel="noreferrer" href={props.git} className={[socialStyles.git, socialStyles.button].join(" ")}>Git</a>}
+            {isTwitter && <a target="_blank" rel="noreferrer" href={props.twitter} className={[socialStyles.twitter, socialStyles.button].join(" ")}>Twitter</a>}
         </div>
     )
 }
