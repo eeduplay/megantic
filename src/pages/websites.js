eccb151768b5e9ac6b7ae8b67fdad85100141f62
Gur <gurlal5231@gmail.com>
2023-06-26 18:18:01 -0400

    Other Websites Updated

--- src/pages/websites.js
+++ src/pages/websites.js
@@ -1,16 +1,58 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Websites(props) {
   return (
     <Layout current={props.location.pathname}>
       <main>
         <h2>Other Websites</h2>
-        <p>https://irg.space/</p>
-        <p>https://www.centauri-dreams.org/</p>
-        <p>https://i4is.org/</p>
-        <p>https://tauzero.aero/</p>
-        <p>https://www.deepspace.ucsb.edu/projects/starlight</p>
+
+        <p>Centauri Dreams</p>
+        <a href="https://www.centauri-dreams.org/" target="_blank" rel="noreferrer"> 
+        <StaticImage 
+        src="../images/content/other-website-logo/centauri-dreams-logo.jpg" 
+        width="400px"
+        alt="Centauri Dreams"
+        />
+        </a>
+  
+        <p>Interstellar Research Group</p>
+        <a href="https://irg.space/" target="_blank" rel="noreferrer"> 
+        <StaticImage 
+        src="../images/content/other-website-logo/IRG-space-logo.jpg" 
+        width="200px"
+        alt="IRG Space"
+        />
+        </a>
+
+        <p>Initiative for Interstellar Studies</p>
+        <a href="https://i4is.org/" target="_blank" rel="noreferrer"> 
+        <StaticImage 
+        src="../images/content/other-website-logo/I4IS-logo.jpg" 
+        width="200px"
+        alt="I4IS"
+        />
+        </a>
+
+        <p>The Tau Zero Foundation</p>
+        <a href="https://tauzero.aero/" target="_blank" rel="noreferrer"> 
+        <StaticImage 
+        src="../images/content/other-website-logo/TauZero-foundation-logo.jpg" 
+        width="200px"
+        alt="TauZero"
+        />
+        </a>
+
+        <p>UCSB Experimental Cosmology Group</p>
+        <a href="https://www.deepspace.ucsb.edu/projects/starlight/" target="_blank" rel="noreferrer"> 
+        <StaticImage 
+        src="../images/content/other-website-logo/UCSB-logo.jpg" 
+        width="200px"
+        height="300px"
+        alt="USCB"
+        />
+        </a>
       </main>
     </Layout>
   )
